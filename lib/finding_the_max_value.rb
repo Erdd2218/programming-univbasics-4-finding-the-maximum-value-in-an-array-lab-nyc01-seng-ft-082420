c6933f387297b23e4545322fba2ab51c190b91b0
Andre Pierre <andre.laurence94@gmail.com>
2020-08-12 00:39:39 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,13 @@
 def find_max_value(array)
   i = 0 
+  maxValue = 0 
   
+  while i < array.length do 
+    if maxValue < array[i]
+      maxValue = array[i]
+    end
+    i += 1 
+  end 
+  
+  maxValue
 end
